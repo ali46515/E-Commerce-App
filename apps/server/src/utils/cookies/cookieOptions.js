7020c5019cb89f158abd5c9aed6b0a3173b5d33c
ali46515 <aliarshad.9474@gmail.com>
2026-05-29 20:11:21 +0500

    Create cookies and token utilities

---
+++ apps/server/src/utils/cookies/cookieOptions.js
@@ -0,0 +1,19 @@
+export const accessCookieOptions = {
+  httpOnly: true,
+
+  secure: process.env.NODE_ENV === "production",
+
+  sameSite: "strict",
+
+  maxAge: 15 * 60 * 1000,
+};
+
+export const refreshCookieOptions = {
+  httpOnly: true,
+
+  secure: process.env.NODE_ENV === "production",
+
+  sameSite: "strict",
+
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+};
