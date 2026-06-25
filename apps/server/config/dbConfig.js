043d1ff5580542aac397d4be3d897e88d5997214
ali46515 <aliarshad.9474@gmail.com>
2026-06-25 20:27:36 +0500

    Update db connection

--- apps/server/config/dbConfig.js
+++ apps/server/config/dbConfig.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
 
     mongoose.connection.on("error", (err) => {
       console.error("MongoDB connection error:", err);
