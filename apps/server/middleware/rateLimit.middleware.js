f8200c1cc9fa6f87f63d056efa9d4353d5d39916
ali46515 <aliarshad.9474@gmail.com>
2026-06-25 18:30:06 +0500

    Implement rate limiting moddleware

---
+++ apps/server/middleware/rateLimit.middleware.js
@@ -0,0 +1,19 @@
+import { rateLimit } from "express-rate-limit";
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: "Too many login attempts, please try again after 15 minutes",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+export { authLimiter, loginLimiter };
